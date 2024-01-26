a831de57f6df575f3fe293a966257da9276f1fa2
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-01-26 17:35:16 -0400

    feat(index.js): import views

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 // En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
+import {Welcome} from './views/Welcome.js';
+import {Home} from './views/Home.js';
+import {DetailCard} from './views/DetailCard.js';
+import {Error} from './views/Error.js';
+import { setRoutes, setRootEl , onURLChange , renderView} from './router.js';
 
-/*
-import Example from './views/Example.js';
 
-Ejemplo de definición de rutas:
-
-const routes = {
-    "/": Example,
-    ...
-}
-*/
 
 /*
 TODO:
@@ -17,3 +13,38 @@ TODO:
 2.- Pasar "root element" a router.
 3.- Invocar el router para renderizar la vista correcta.
 */
+const routes = {
+    "/": Welcome,
+    "/home": Home,
+    "/detailCard":DetailCard,
+    "/error":Error
+
+}
+setRoutes(routes);
+console.log(setRoutes(routes));
+const root=document.getElementById("root");
+renderView(routes,"hola");
+
+//DOMContentLoaded no espera a que las hojas de estilo se carguen, sin embargo los scripts diferidos sí esperan hojas de estilo, y el DOMContentLoadedevento está hecho cola después de guiones diferidos. También, guiones que no se aplazan o se sincronizado (por ejemplo. <script>) esperará a que las hojas de estilo ya parladadas se carguen.
+// window.addEventListener("DOMContentLoaded", ()=>{
+//     console.log(setRootEl());
+    
+//     onURLChange();
+    
+//    console.log("root",setRootEl(root));
+
+// });
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log(setRootEl(root));
+    
+    onURLChange();
+    
+   console.log("root",setRootEl(root));
+
+});
+// POPSATE
+window.addEventListener("popstate",()=>{
+
+ });
+
+
