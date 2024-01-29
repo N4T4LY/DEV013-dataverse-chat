4d7e614d29ca7b83fe3909acec1cc70642cd84d3
Aylin <aylin_santacruz@hotmail.com>
2024-01-29 15:22:10 -0500

    test(dataFunctions.spec.js): Add sortData test

--- src/test/dataFunctions.spec.js
+++ src/test/dataFunctions.spec.js
@@ -1,1 +1,45 @@
-import { filterData, sortData, computeStats } from '../src/dataFunctions.js';
+import { filterData, sortData, computeStats } from "../src/dataFunctions.js";
+
+describe("SORTDATA", () => {
+  // 1. Fn existe
+  it("SORTDATA EXISTS", () => {
+    expect(typeof sortData).toBe("function");
+  });
+  // 2. fn funciona (comportamiento deseado)
+  it("SORT ASCENDING", () => {
+    const TEST_SORT = [
+      { name: "Bulsaur" },
+      { name: "Abra" },
+      { name: "Caterpie" },
+    ];
+    const sortBy = "name";
+    const sortOrder = "asc";
+    expect(sortData(TEST_SORT, sortBy, sortOrder)).toEqual([
+      { name: "Abra" },
+      { name: "Bulsaur" },
+      { name: "Caterpie" },
+    ]);
+  });
+  it("SORT DESCENDING", () => {
+    const TEST_SORT = [
+      { name: "Bulsaur" },
+      { name: "Abra" },
+      { name: "Caterpie" },
+    ];
+    const sortBy = "name";
+    const sortOrder = "desc";
+    expect(sortData(TEST_SORT, sortBy, sortOrder)).toEqual([
+      { name: "Caterpie" },
+      { name: "Bulsaur" },
+      { name: "Abra" },
+    ]);
+  });
+  // 3. Estresar al test
+  it("DATA EMPTY", () => {
+    const TEST_SORT = [];
+    const sortBy = "name";
+    const sortOrder = "asc";
+    // expect(sortData(TEST_SORT, sortBy, sortOrder )).toBe(false)
+    expect(sortData(TEST_SORT, sortBy, sortOrder)).toBeFalsy();
+  });
+});
