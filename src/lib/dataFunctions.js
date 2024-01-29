f9833c8780964b367297501343b4992a35b8f87f
Aylin <aylin_santacruz@hotmail.com>
2024-01-29 15:18:37 -0500

    feat(dataFunctions.js): Add computeStats  functions

--- src/lib/dataFunctions.js
+++ src/lib/dataFunctions.js
@@ -21,8 +21,35 @@ export const sortData = (data, sortBy, sortOrder) => {
   return sortedData;
 };
 
-
 export const filterData = (data, filterBy, value) => {
-    const result = data.filter((type) => type.type[filterBy].includes(value));
-    return result;
+  const result = data.filter((type) => type.type[filterBy].includes(value));
+  return result;
+};
+
+export const computeStats = (originalData) => {
+  let names = [];
+  // eslint-disable-next-line no-undef
+  const uniqueType = new Set();
+
+  originalData.forEach((data) => {
+    const types = data.type.typeName;
+    types.forEach((el) => {
+      uniqueType.add(el);
+    });
+  });
+
+  names = Array.from(uniqueType);
+
+  const nroPokemons = names.map((typeName) =>
+    originalData.reduce((accumulator, pokemon) => {
+      if (pokemon.type.typeName.includes(typeName)) {
+        accumulator += 1;
+      }
+      return parseInt(accumulator);
+    }, 0)
+  );
+  return {
+    names,
+    nroPokemons,
   };
+};
