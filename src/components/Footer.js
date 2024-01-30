3352e01259dc6fee4192f47dd6204e62af331dc3
Aylin <aylin_santacruz@hotmail.com>
2024-01-30 14:40:26 -0500

    feat(Footer.js)Create footer element

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export const Footer = () =>{
+    const footer = document.createElement('footer');
+    footer.innerHTML = `<p>Copyright © 2023 Nataly Fernandez y Aylin Santa Cruz Vargas</p>`;
+    return footer;
+}
