e0bd317c79adb1d0fdc22e6ff85c7fa37b4fd90b
Aylin <aylin_santacruz@hotmail.com>
2024-01-30 14:39:00 -0500

    feat(Header.js):Create header element

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export const Header = () => {
+    const header = document.createElement("header");
+    header.innerHTML = `<h1>P<img src="assets/pokemones/pokebola.png" alt="" />kéDex</h1>`;
+    return header;
+}
