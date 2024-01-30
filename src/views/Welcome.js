8216eae95b9a1227c116ef94494c8e7a426ea6d5
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-01-30 16:08:45 -0400

    feat(welcome.js): Create welcome view

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,11 +1,43 @@
+import { navigateTo }  from "../router.js";
 export const Welcome = () => {
-  const main = document.createElement("main");
-  const containerLeft = document.createElement("section");
-  const containerRight = document.createElement("section");
+  const main = document.createElement("div");
+  const containerLeft = document.createElement("div");
+  const polygon = document.createElement("div");
+  const img =document.createElement('img');
+  const containerRight = document.createElement("div");
+  const  h1 = document.createElement("h1");
+  const p=document.createElement("p")
+  const button=document.createElement("button");
+  const footer=document.createElement("p");
 
   main.setAttribute("id", "welcoming");
-  main.innerHTML = "I'm the Welcome Page";
-  return main;
-  //return document.getElementById("root").innerHTML=`<h1>BIENVENIDA</h1>`;
+  containerLeft.setAttribute("class","image");
+  polygon.setAttribute("class","polygon")
+  img.setAttribute("src","./assets/pokemons-welcome.png")
+  containerRight.setAttribute("class","text");
+  button.setAttribute("id","close-welcoming");
+  footer.setAttribute("class","footer");
 
+  h1.innerHTML = "¡Bienvenidos a PokéDex!";
+  p.innerHTML = "¡Encuentra a tu Pokémon!";
+  button.innerHTML = "INICIAR";
+  footer.innerHTML="Copyright © 2023 Nataly Fernandez y Aylin Santa Cruz Vargas";
+  
+  //containerLeft
+ containerLeft.appendChild(polygon);
+ containerLeft.appendChild(img);
+  //containerRight
+  containerRight.appendChild(h1);
+  containerRight.appendChild(p);
+  containerRight.appendChild(button);
+  containerRight.appendChild(footer);
+
+  main.appendChild(containerLeft);
+  main.appendChild(containerRight);
+  
+  //return document.getElementById("root").innerHTML=`<h1>BIENVENIDA</h1>`;
+  button.addEventListener("click",()=>{
+    navigateTo("/home",{});
+    })
+return main;
 }
