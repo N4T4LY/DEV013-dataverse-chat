43b56834154481c37c8e4d819eeca49cb0c78621
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-02-02 15:21:50 -0400

    feat(detailCard.js): import dataset and interpolate data to html

--- src/views/DetailCard.js
+++ src/views/DetailCard.js
@@ -1,7 +1,7 @@
 import { navigateTo } from "../router.js";
-export const DetailCard = () => {
-  // let html='';
-  // return(html+=`<h1>BIENVENIDA</h1>`);
+import data from "../data/dataset.js"
+export const DetailCard = (pokemon) => {
+    
   const main = document.createElement("main");
   const polygon = document.createElement("div");
   const containerLeft = document.createElement("section");
@@ -10,40 +10,39 @@ export const DetailCard = () => {
   polygon.setAttribute("class", "poligon");
   containerLeft.setAttribute("class", "containerLeft");
   containerRight.setAttribute("class", "containerRight");
+
   containerLeft.innerHTML = `
     <div class="chatPersonal">
         <i class="fa-brands fa-rocketchat"></i>
     </div>
-    <img src="./assets/pokemones/Poliwrath.png" alt="">
+    <img src="${pokemon.image}" alt="${pokemon.name}">
     <div class="baseStats">
         <i class="fa-solid fa-chart-simple"></i>
     </div>`;
   containerRight.innerHTML = `
   <div class="recoilArrow"><i class="fa-solid fa-arrow-left"></i></div>
-  <h1>Bulbasaur</h1>
-    <p>Este Pokémon nace con una semilla en el lomo, que brota 
-        con el paso del tiempo.
-    </p>
+  <h1>${pokemon.name}</h1>
+    <p>${pokemon.description}</p>
     <div class="table">
         <table>
             <tr>
                 <th class="altura">Altura</th>
-                <td class="datoP">0,7 M</td>
+                <td class="datoP">${pokemon.heigth} M</td>
             </tr>
             <tr>
                 <th class="peso">Peso</th>
-                <td>6,9 Kg</td>
+                <td>${pokemon.weight} Kg</td>
             </tr>
         </table>
 
         <table>
             <tr>
                 <th class="altura">Categoria</th>
-                <td class="datoP">Semilla</td>
+                <td class="datoP">${pokemon.category}</td>
             </tr>
             <tr>
                 <th class="peso">Habilidad</th>
-                <td >Espesura</td>
+                <td>${pokemon.ability}</td>
             </tr>
         </table>
     </div>
