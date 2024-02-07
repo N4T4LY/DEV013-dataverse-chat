ec7158ba4c831b2527574273d479b3ccf4413813
Aylin <aylin_santacruz@hotmail.com>
2024-02-07 00:22:34 -0500

    feat(error.js): Add elements in error.js

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,6 +1,17 @@
+import { navigateTo } from "../router.js";
 export const Error = () => {
-
-  const el = document.createElement("h1");
-  el.innerHTML = "Error";
-  return el;
-}
+  const containerError = document.createElement("main");
+  const title = document.createElement("h2");
+  const buttonBack = document.createElement("button");
+  containerError.setAttribute("class", "containerError");
+  buttonBack.setAttribute("class", "buttonBack");
+  title.textContent = `Pagina no encontrada`;
+  buttonBack.textContent = "Volver al Inicio";
+  containerError.append(title, buttonBack);
+  const backHome = containerError.querySelector(".buttonBack");
+  console.log(backHome);
+  backHome.addEventListener("click", () => {
+    navigateTo("/home", {});
+  });
+  return containerError;
+};
