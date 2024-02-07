3151018c8c57fa1b1b97cff356832407e9fd3079
Aylin <aylin_santacruz@hotmail.com>
2024-02-07 08:16:25 -0500

    feat(error.js):Add styles and elements new

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,13 +1,17 @@
 import { navigateTo } from "../router.js";
 export const Error = () => {
   const containerError = document.createElement("main");
+  const error = document.createElement("h1");
   const title = document.createElement("h2");
   const buttonBack = document.createElement("button");
   containerError.setAttribute("class", "containerError");
+  title.setAttribute("class", "titleNotFound");
   buttonBack.setAttribute("class", "buttonBack");
+  error.setAttribute("class", "title404");
+  error.innerHTML = `4<img class="error404" src="assets/pokemones/Pokebola404.png"/>4`
   title.textContent = `Pagina no encontrada`;
   buttonBack.textContent = "Volver al Inicio";
-  containerError.append(title, buttonBack);
+  containerError.append(error,title, buttonBack);
   const backHome = containerError.querySelector(".buttonBack");
   console.log(backHome);
   backHome.addEventListener("click", () => {
