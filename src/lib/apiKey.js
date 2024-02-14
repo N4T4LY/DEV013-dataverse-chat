a22065ef3e9fc39bb5bca59433eb9d2e5deb976a
Aylin <aylin_santacruz@hotmail.com>
2024-02-13 23:56:30 -0500

    fix(apiKey.js):Add getApi y setApi function

--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -1,25 +1,12 @@
-export const getApiKey = (prompt) => {
+export const getApiKey = () => {
     // Implementa el código para obtener la API KEY desde Local Storage
-    const APIKEY = "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"
-    console.log(APIKEY)
-    const response = fetch(`https://api.openai.com/v1/chat/completions`,{
-        method:"POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + APIKEY
-        },
-        body: JSON.stringify({
-            model:'gpt-3.5-turbo',
-            message: [
-                {
-                    "role":"system",
-                    "content": prompt
-                }
-            ]
-        })
-    })
-    return response;
+    // const APIKEY = "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";
+    const APIKEYGET = localStorage.getItem("apiKey");
+    return APIKEYGET;
  };
  export const setApiKey = (key) => {
    // Implementa el código para guardar la API KEY en Local Storage
+   const APIKEY = localStorage.setItem("apiKey",key);
+   return APIKEY;
+
  };
