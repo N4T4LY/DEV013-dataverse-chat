21b7613fc7f78a2755e7e94e03595265e0f795a1
Aylin <aylin_santacruz@hotmail.com>
2024-02-16 14:08:25 -0500

    fix(openAIApi.js):testing api functionality

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,23 +1,33 @@
-const openAI = require('openai');
-const openai = new openAI();
-export const communicateWithOpenAI = (messages) => {
+// import OpenAI from "openai";
+
+// const openai = new OpenAI();
+const communicateWithOpenAI = (messages) => {
     //Aquí es donde debes implementar la petición con fetch o axios
     const response = fetch(`https://api.openai.com/v1/chat/completions`,{
         method:"POST",
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + APIKEY
+            'Authorization': 'Bearer ' + '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'
         },
         body: JSON.stringify({
             model:'gpt-3.5-turbo',
-            message: [
+            messages: [
                 {
                     "role":"system",
-                    "content": messages
+                    "content":`Comportate como ${messages}`
                 }
             ]
         })
     })
     return response;
  };
+
+ console.log(communicateWithOpenAI("Charizard").then((res)=>{
+    console.log(res.json().then((data) =>{
+        console.log(data.choices[0].message)
+    }).catch(() => {
+        console.log("error")
+    }))
+ }))
  
+ //Consumo de Promesas
