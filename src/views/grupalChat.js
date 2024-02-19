de3dd4b19b2fcf82a239d37e79eb2742548ee0bc
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-02-19 17:42:51 -0400

    feat(grupalChat.js): change grupalChat into to components

--- src/views/grupalChat.js
+++ src/views/grupalChat.js
@@ -1,40 +1,21 @@
+import { InputChat } from "../components/InputChat.js";
+import { BubblesChat } from "../components/BubblesChat.js";
 export const GrupalChat=()=>{
     const main =document.createElement("main");
+    const section = document.createElement("section");
     main.setAttribute( "id","grupal-chat" );
+    section.setAttribute( "id","inputChat" );
+    section.appendChild((InputChat()))
+
     main.innerHTML=`
     <section id="iconschat">
         <div class="recoilArrow"><i class="fa-solid fa-arrow-left"></i></div>
         <div class="chatIcon"><i class="fa-solid fa-comments fa-2xl"></i></div>
     </section>
-    
-    <section id="chatG">
-        <div id="userC">
-            <h3 id="userName">Tú</h3>
-            <div id="userImage">
-                <i class="fa-solid fa-user"></i>
-            </div>
-        
-        
-        </div> 
-        <div id="answerChatU">
-        <p>"Hola, Charmander. Cuéntame más sobre tus habilidades."</p></div>
-        
-        <div id="pokemonC">
-        
-            <div id="pokemonImage">
-                <img src="../assets/pokemones/Charmander.png" alt="">
-            </div>
-            <h3 id="pokemonName">Charmander</h3>
-        </div>
-        <div class="answerChatP"><p>Charmander (AI de OpenAI): "¡Hola! Claro, estaré encantada de hablar sobre eso. Mi habilidad es el fuego. Además, potencia mis movimientos de tipo Fuego cuando me quedan poco PS. ¿Hay algo más en particular que te gustaría saber?"</p></div>
-    </section>    
-    <section id="inputChat">
-            <input type=text id="inputGrupal" placeholder="Escribe tu mensaje">
-            <button id="sendButton"><img src="../assets/send.png" alt=""></button>
-    
-    </section>
-   
     `
+    main.appendChild(BubblesChat("ads","sadsdsd"));
+    main.appendChild(section);
+
     const recoilArrow = main.querySelector(".recoilArrow");
     recoilArrow.addEventListener("click", () => {
         //navigateTo("/home", {});
