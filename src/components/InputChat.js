c99dc7ef84244580764b3a8fd4fcb8057c6b3cab
Aylin <aylin_santacruz@hotmail.com>
2024-02-20 15:22:45 -0500

    fix:modify some code lines

--- src/components/InputChat.js
+++ src/components/InputChat.js
@@ -1,17 +1,33 @@
+
+
 export const InputChat = () => {
-  const form = document.createElement("form");
+  const form = document.createElement("article");
   const input = document.createElement("input");
   const buttonSend = document.createElement("button");
+  input.id = "chat-input";
+  buttonSend.id = "send-message";
+  form.setAttribute("class", "inputChatContainer");
   input.setAttribute("class", "inputChat");
   input.setAttribute("placeholder", "Escribe tu mensaje");
   input.setAttribute("type", "text");
-  buttonSend.id = "send-message";
-  buttonSend.setAttribute("type", "submit");
+  buttonSend.setAttribute("class","sendMessage")
+  //   buttonSend.setAttribute("type", "submit");
   buttonSend.innerHTML = `<ion-icon name="send"></ion-icon>`;
 
-  form.setAttribute("class", "inputChatContainer");
   form.appendChild(input);
   form.appendChild(buttonSend);
+
+//   buttonSend.addEventListener("click", () => {
+//     // console.log(input.value);
+//     communicateWithOpenAI(pokemon.name, input.value)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         BubblesChat(input.value, data.choices[0].message.content);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   });
 
   return form;
 };
