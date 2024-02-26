bf7c77c9fdea3fc4a50c40b46b91bbcbcf1fe6c6
Aylin <aylin_santacruz@hotmail.com>
2024-02-26 12:38:10 -0500

    feat(Home.js):validate if apikey exists

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ import { Filters } from "../components/Filters.js";
 import { Footer } from "../components/Footer.js";
 import { sortData, filterData, computeStats } from "../lib/dataFunctions.js";
 import { navigateTo } from "../router.js";
-import { setApiKey} from "../lib/apiKey.js";
+import { setApiKey, getApiKey} from "../lib/apiKey.js";
 import { modalApi } from "../components/ModalApi.js";
 
 export const Home = () => {
@@ -34,27 +34,30 @@ export const Home = () => {
   const close = newContainer.querySelector(".fa-xmark");
 
   buttonChat.addEventListener("click", () => {
-    // navigateTo("/grupalChat", {});
-    newContainer.appendChild(modalApi("/grupalChat"));
-    const modalKey = newContainer.querySelector(".modalKey");
-    const closeModal = newContainer.querySelector(".cancel");
-    closeModal.addEventListener("click", () => {
-      modalKey.remove();
+    if(!getApiKey()){
+      newContainer.appendChild(modalApi());
+      const modalKey = newContainer.querySelector(".modalKey");
+      const closeModal = newContainer.querySelector(".cancel");
+      closeModal.addEventListener("click", () => {
+        modalKey.remove();
+        overlay.classList.toggle("overlay-active");
+      });
+      modalKey.addEventListener("submit", () => {
+        // e.preventDefault();
+        const sendApi = newContainer.querySelector(
+          "input[type ='password']"
+        ).value;
+        // localStorage.setItem("sendApi",sendApi)
+        if (sendApi) {
+          setApiKey(sendApi);
+          navigateTo("/grupalChat", {});
+          // modalKey.remove();
+        }
+      });
       overlay.classList.toggle("overlay-active");
-    });
-    modalKey.addEventListener("submit", () => {
-      // e.preventDefault();
-      const sendApi = newContainer.querySelector(
-        "input[type ='password']"
-      ).value;
-      // localStorage.setItem("sendApi",sendApi)
-      if (sendApi) {
-        setApiKey(sendApi);
-        navigateTo("/grupalChat", {});
-        // modalKey.remove();
-      }
-    });
-    overlay.classList.toggle("overlay-active");
+    }else{
+      navigateTo("/grupalChat", {});
+    }
   });
 
   modal.addEventListener("click", () => {
