d6b80a66d96d92ace1d81ee1cd1becc7a6769366
Aylin <aylin_santacruz@hotmail.com>
2024-02-26 15:02:12 -0500

    test:implementing the tests for the apikey.js

---
+++ test/apiKey.spec.js
@@ -0,0 +1,29 @@
+import { getApiKey, setApiKey } from "../src/lib/apiKey.js";
+
+describe("getApiKey", () => {
+  it("should return the value of the API key", () => {
+    const apiKey = "1284jdfdhdus";
+    setApiKey(apiKey);
+    expect(getApiKey()).toBe("1284jdfdhdus");
+  });
+
+  it("should return false if there is no api key", () => {
+    const apiKey = "";
+    setApiKey(apiKey);
+    expect(getApiKey()).toBeFalsy();
+  });
+});
+
+describe("setApiKey", () => {
+  it("should set the API Key correctly", () => {
+    const apiKey = "1234";
+    console.log(setApiKey(apiKey));
+    expect(localStorage.getItem('apiKey')).toBe(apiKey);
+  });
+
+  it("should return false if the API is not saved", () => {
+    const apiKey = "";
+    console.log(setApiKey(apiKey));
+    expect(localStorage.getItem('apiKey')).toBeFalsy();
+  });
+});
