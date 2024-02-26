93f97e4a146faf872744a37b3fdeb834d669976a
Aylin <aylin_santacruz@hotmail.com>
2024-02-26 15:03:37 -0500

    test:changed the location of the dataFunction import

--- src/test/Footer.spec.js
+++
@@ -1,7 +0,0 @@
-// import { Footer } from "../components/Footer.js";
-
-// describe("Footer Component",() =>{
-//     test('the paragraph is in the document',() =>{
-//         expect(Footer()).toBeInTheDocument();
-//     })
-// })
