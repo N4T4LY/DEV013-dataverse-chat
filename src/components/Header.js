92cb72d5b2284f97e5bac170ea3cf5e2dcfca4ed
N4T4LY <55033104+N4T4LY@users.noreply.github.com>
2024-03-04 12:19:54 -0400

    fix(header.js): adding alt on pokebola image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 export const Header = () => {
   const header = document.createElement("header");
-  header.innerHTML = `<h1>P<img class="pokebolaHeader" src="assets/pokemones/pokebola.png" alt="" />kéDex</h1>`;
+  header.innerHTML = `<h1>P<img class="pokebolaHeader" src="assets/pokemones/pokebola.png" alt="pokebola"/>kéDex</h1>`;
   return header;
 };
