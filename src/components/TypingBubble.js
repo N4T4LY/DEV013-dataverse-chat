f11908d42a1c9ebae6b43c871fad7b702a541b3d
Aylin <aylin_santacruz@hotmail.com>
2024-03-05 23:02:17 -0500

    feat(TypingBubble.js): Add Typing component

---
+++ src/components/TypingBubble.js
@@ -0,0 +1,6 @@
+export const TypingBubble = () => {
+  const typingBubble = document.createElement("div");
+  typingBubble.textContent = "Esta Escribiendo...";
+  typingBubble.id = "typing-bubble";
+  return typingBubble;
+};
